7e5f809ffd6fba10469c18e0aa5e721e2d07c2e0
MarlondDriza <dragon5trainer@outlook.com>
2022-01-09 10:36:22 +0800

    small change

--- examples/simple-connect/index.js
+++ examples/simple-connect/index.js
@@ -127,48 +127,48 @@ ReactDOM.render(
   >
     <App />
     <style>
-      {`
-        body {
-          width: 40rem;
-          margin: 0 auto;
-          font-family: sans-serif;
-          font-size: 18px;
-          line-height: 1.5;
-        }
-        button {
-          width: 6rem;
-        }
-        h1 {
-          font-weight: 400;
-        }
-        p {
-          display: grid;
-          justify-content: space-between;
-          align-items: center;
-          grid-auto-flow: column;
-          gap: 1rem;
-          align-items: center;
-          margin: 2rem 0;
-        }
-        button {
-          height: 3rem;
-          cursor: pointer;
-          font-size: 1rem;
-          padding: 0;
-        }
-        .connect-label {
-          margin-bottom: 1rem;
-        }
-        .connect-buttons {
-          display: grid;
-          gap: 10px;
-          grid-template-columns: repeat(auto-fill, minmax(140px, 1fr));
-        }
-        .connect-buttons button {
-          width: 100%;
-          height: 4rem;
-        }
-      `}
+      // {`
+      //   body {
+      //     width: 40rem;
+      //     margin: 0 auto;
+      //     font-family: sans-serif;
+      //     font-size: 18px;
+      //     line-height: 1.5;
+      //   }
+      //   button {
+      //     width: 6rem;
+      //   }
+      //   h1 {
+      //     font-weight: 400;
+      //   }
+      //   p {
+      //     display: grid;
+      //     justify-content: space-between;
+      //     align-items: center;
+      //     grid-auto-flow: column;
+      //     gap: 1rem;
+      //     align-items: center;
+      //     margin: 2rem 0;
+      //   }
+      //   button {
+      //     height: 3rem;
+      //     cursor: pointer;
+      //     font-size: 1rem;
+      //     padding: 0;
+      //   }
+      //   .connect-label {
+      //     margin-bottom: 1rem;
+      //   }
+      //   .connect-buttons {
+      //     display: grid;
+      //     gap: 10px;
+      //     grid-template-columns: repeat(auto-fill, minmax(140px, 1fr));
+      //   }
+      //   .connect-buttons button {
+      //     width: 100%;
+      //     height: 4rem;
+      //   }
+      // `}
     </style>
   </UseWalletProvider>,
   document.querySelector('#app')
